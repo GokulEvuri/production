762567ea5a2339a224788c87663562dcd62c0244
Gokul <gokul@evuri.com>
2014-01-20 13:27:37 +0100

    registration marked done

--- livestreetmedia.com/livestreetmedia.com/resturant/api/customer/registration.php
+++ livestreetmedia.com/livestreetmedia.com/resturant/api/customer/registration.php
@@ -2,23 +2,26 @@
 	include_once('../../includes/connect_database.php'); 
 	include_once('../../variables/variables.php');
 	
-	if(isset($_POST['accesskey']) && isset($_POST['username'])&& isset($_POST['password'])&& isset($_POST['email'])) {
-		$access_key_received = $_POST['accesskey'];
-		$username = $_POST['username'];
-		$password = $_POST['password'];
-		$email =  $_POST['email'];
+	if(isset($_GET['accesskey']) && isset($_GET['username'])&& isset($_GET['password'])&& isset($_GET['email'])) {
+		$access_key_received = $_GET['accesskey'];
+		$username = $_GET['username'];
+		$password = $_GET['password'];
+		$email =  $_GET['email'];
 
 		if($access_key_received == $access_key){
 				// find menu by category id in menu table
 				$sql_query = "SELECT * FROM tbl_customers WHERE UserName = '$username'";
 				$result = $connect->query($sql_query) or die("Error : ".mysql_error());
-	
-			if(empty($result->num_rows)){
-					
+			echo $result->num_rows;
+			if($result->num_rows == 0){
+					echo "Insert here";
 				// create new user
-			$sql_query = "INSERT INTO tbl_customers (UserName,Password,Email) VALLUES ('$username','$password','$email')";
-				$result = $connect->query($sql_query) or die("Error : ".mysql_error());
-	
+			$sql_query = "INSERT INTO tbl_customers (UserName,Password,Email) VALUES ('$username','$password','$email')";
+			
+			$result = $connect->query($sql_query) or die("Error : ".mysql_error());
+		
+			echo $result;
+			
 			}
 			else{
 				echo "Requested user name is unavailable";
@@ -30,5 +33,5 @@
 	} else {
 		die('accesskey, username, password and email are required.');
 	}
-	include_once('../includes/close_database.php'); 
+	include_once('../../includes/close_database.php'); 
 ?>
